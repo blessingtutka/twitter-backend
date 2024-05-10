2f8cba630abafd755fcb2f80fa20228c1b60082b
blessingtutka <blessingtutka298@gmail.com>
2024-05-10 12:20:36 +0200

    Setup Cors

--- api.js
+++ api.js
@@ -2,13 +2,14 @@ import express from 'express'
 import dotenv from 'dotenv'
 import tweetRoutes from './routers/tweets.routes.js'
 import userRoutes from './routers/user.routes.js'
-
+import cors from 'cors'
 // Env Varaibles
 dotenv.config()
 const { PORT } = process.env
 
 // Config
 const app = express()
+app.use(cors());
 app.use(express.json())
 
 // Routes
